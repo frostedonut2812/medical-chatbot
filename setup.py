39cff50b0cb61f829f9da61cd8fb23f9a98dda0f
frostedonut2812 <72467727+frostedonut2812@users.noreply.github.com>
2026-06-24 18:12:28 +0530

    Initial commit: Medical chatbot with RAG architecture

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sachin Chopra",
+    author_email="sachinchopra1228@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "You are a medical assistant for question answering tasks"
+    "Use the following pieces of retreived content to answer"
+    "if you dont know say that you dont know"
+    "answer in 1 para"
+    "\n\n"
+    "{context}"
+)
